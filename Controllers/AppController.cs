843505cf8f831551a885e5fc6e6aaaf7db8ccec5
Henk Boelman <henk@boelman.email>
2021-05-04 17:11:18 +0200

    Init

---
+++ Controllers/AppController.cs
@@ -0,0 +1,90 @@
+﻿using CustomVisionTFJS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomVisionTFJS.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly ILogger<AppController> _logger;
+
+        public AppController(ILogger<AppController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (System.IO.File.Exists("wwwroot/models/model.json")) {
+
+                return RedirectToAction("camera");
+            }
+
+
+            return RedirectToAction("upload");
+        }
+
+        public IActionResult Camera()
+        {
+            if (!System.IO.File.Exists("wwwroot/models/model.json"))
+            {
+                return RedirectToAction("upload");
+            }
+            return View();
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult UploadModel(IFormFile image)
+        {
+            string basePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/models");
+
+            if (image != null && Path.GetExtension(image.FileName)==".zip")
+            {
+                //Set Key Name
+                string ImageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                string SavePath = Path.Combine(basePath, ImageName);
+
+                if (Directory.Exists(basePath)) {
+                    Directory.Delete(basePath, true);
+                }
+                
+                Directory.CreateDirectory(basePath);
+
+                using (var stream = new FileStream(SavePath, FileMode.Create))
+                {
+                    image.CopyTo(stream);
+                }
+                ZipFile.ExtractToDirectory(SavePath, basePath);
+
+                return RedirectToAction("camera");
+            }
+
+            // Clean up the uploaded files if anything went wrong
+            if (Directory.Exists(basePath))
+            {
+                Directory.Delete(basePath, true);
+            }
+            return View();
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,79 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const threshold = 0.75;
+const label_file = '/models/labels.txt'
+const model_file = '/models/model.json'
+
+async function load_labels() {
+    return fetch(label_file)
+        .then(function (response) {
+            return response.text();
+        }).then(function (data) {
+            var lines = data.split(/\n/);
+            return lines;
+        })
+}
+
+async function load_model() {
+    const model = new cvstfjs.ClassificationModel();
+    await model.loadModelAsync(model_file);
+    return model;
+}
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time))
+}
+
+predict = (video, model, labels) => {
+
+    model.executeAsync(video).then(predictions => {
+        this.renderPredictions(predictions, labels);
+        sleep(1000).then(() => {
+            this.predict(video, model, labels);
+        })
+    });
+
+};
+
+renderPredictions = (predictions, labels) => {
+    let pred = predictions[0]
+    if (Math.max(...pred) > threshold) {
+        document.getElementById("label").innerHTML = labels[pred.indexOf(Math.max(...pred))];
+    } else {
+        document.getElementById("label").innerHTML = "unkown"
+    }
+};
+
+if (document.querySelector("#frame") && navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    this.videoRef = document.querySelector("#frame");
+
+    const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+            audio: false,
+            video: {
+                facingMode: "user"
+            }
+        })
+        .then(stream => {
+            window.stream = stream;
+            this.videoRef.srcObject = stream;
+            return new Promise((resolve, reject) => {
+                this.videoRef.onloadedmetadata = () => {
+                    resolve();
+                };
+            });
+        });
+
+    const modelPromise = load_model();
+    const labelPromise = load_labels();
+
+    Promise.all([webCamPromise, modelPromise, labelPromise])
+        .then(values => {
+           this.predict(this.videoRef, values[1], values[2]);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
